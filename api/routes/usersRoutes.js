5f8a608779f7c4ddb950cf69229b47be935efe92
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2023-12-29 18:54:10 +0530

    new route

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -121,4 +121,25 @@ router.put("/:id/unfollow", async (req, res) => {
     }
 });
 
+//Search Users
+router.get("/searchUsers", async(req,res) => {
+
+  
+        const keyword = req.query.search 
+         ? {
+            $or: [
+                {name: {$regex : req.query.search, $options: "i" }},
+                {email:{$regex : req.query.search, $options: "i" }},
+            ]
+         } 
+         : {} 
+    
+         const users = await User.find(keyword).find({ _id: {$ne : req.body.userId} });
+         res.json(users);
+        
+
+
+})
+
+
 module.exports = router;
